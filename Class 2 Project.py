93979ae7f24c53f5403106f1d1b434aac5929d6a
monroecoding <94639671+monroecoding@users.noreply.github.com>
2022-10-01 10:47:10 -0400

    Add files via upload

---
+++ Class 2 Project.py
@@ -0,0 +1,38 @@
+"""
+Coding Bootcamp at the Monroe Township Library
+
+Class 2 Project
+
+Create a program that allows the user to input a sentence and return
+some data about it.
+
+The data we want:
+    -length of the sentence
+    -the sentence in reverse (if sentence is 'hello world', print 'dlrow olleh')
+    -how many times does the sentence contain the word 'the'
+    -use concatenation or a formatted string to print everything out
+    
+Things to consider:
+    -make sure to consider case (uppercase and lowercase)
+    -using \n inside of string will insert a line break
+    
+Test cases:
+    input -> This is a sentence
+    output -> length: 18
+              reverse: ecnetnes a si sihT
+              'the' count: 0
+              
+    input -> The number of the day is 7
+    output -> length: 26
+              reverse: 7 si yad eht fo rebmun ehT
+              'the' count: 2
+"""
+
+
+sentence = input("Enter your word here: ")
+
+#your code goes here
+
+
+
+
